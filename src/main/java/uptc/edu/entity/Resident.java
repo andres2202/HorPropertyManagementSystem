fea538d33404dee2b18057f66f083d4061dd64b5
andres2202 <andresfelipenf@gmail.com>
2022-12-07 17:34:18 -0500

    entity resident

--- src/main/java/uptc/edu/entity/Resident.java
+++ src/main/java/uptc/edu/entity/Resident.java
@@ -1,5 +1,33 @@
 package uptc.edu.entity;
 
-public class Resident {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "resident")
+public class Resident implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name="id")
+	private String id;
+	
+	@Column
+	private String name;
+	
+	@Column(name="surname")
+	private String surname;
+	
+	@Column(name="cellPhone")
+	private String cellPhone;
+	
+	@Column(name="block")
+	private char block;
+	
+	@Column(name="")
+	private String apartment;
 }
